a8a5a67982fab7369ae95357b2a899ea372cbe3c
Mikhail Matiunin <mikhail.matiunin@doctorina.com>
2025-05-30 16:58:15 +0400

    Implement app initialization logic and loading progress management

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:sw_example/src/initialization.dart';
 
 void main() => runZonedGuarded<void>(
   () async {
-    final binding =
-        WidgetsFlutterBinding.ensureInitialized()..deferFirstFrame();
-    for (var i = 0; i < 100; i++) {
-      // Simulate initialization
-      await Future.delayed(const Duration(milliseconds: 25));
-    }
+    await initializeApp();
     runApp(const App());
-    binding.addPostFrameCallback((_) {
-      binding.allowFirstFrame();
-    });
   },
   (error, stackTrace) =>
       print('Top level exception: $error'), // ignore: avoid_print
